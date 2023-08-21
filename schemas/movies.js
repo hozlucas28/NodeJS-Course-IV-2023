944a4ef7de7287fcd029e9691c74862e81468d0d
hozlucas28 <hozlucas28@gmail.com>
2023-08-21 01:49:31 -0300

    Upload lesson

--- schemas/movies.js
+++ schemas/movies.js
@@ -1,31 +1,25 @@
 /* --------------------------------------------------------------------------
  * APUNTES:
- * 		   En este archivo se muestra un caso de uso de Zod (módulo de
- *         terceros) para verificar los datos entrantes al realizar
- *         una solicitud <POST> a la API.
+ * 		   En este archivo se encuentran desarrolladas las funciones de
+ *         validación del objeto: "Movie", utilizando la librería "zod".
 -------------------------------------------------------------------------- */
 
-const z = require('zod')
+import * as zod from 'zod'
 
-const movieSchema = z.object({
-	title: z.string({
-		invalid_type_error: 'Movie title must be a string',
-		required_error: 'Movie title is required',
-	}),
-	year: z.number().int().min(1900).max(2024),
-	director: z.string(),
-	duration: z.number().int().positive(),
-	rate: z.number().min(0).max(10).default(5.5),
-	poster: z.string().url(),
-	genre: z.array(z.enum(['Action', 'Crime', 'Drama'])),
+const movieSchema = zod.object({
+	director: zod.string(),
+	duration: zod.number().int().positive(),
+	genre: zod.array(zod.enum(['Action', 'Crime', 'Drama'])),
+	poster: zod.string().url(),
+	rate: zod.number().min(0).max(10).default(5.5),
+	title: zod.string(),
+	year: zod.number().int().min(1900).max(2024),
 })
 
-function validateMovie(movie) {
+export function validateMovie(movie) {
 	return movieSchema.safeParse(movie)
 }
 
-function validatePartialMovie(movie) {
+export function validatePartialMovie(movie) {
 	return movieSchema.partial().safeParse(movie)
 }
-
-module.exports = { validateMovie, validatePartialMovie }
